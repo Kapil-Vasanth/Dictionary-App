6b5ade8fb523b090530671959fa8dbd8cfe16c5f
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-08-14 07:38:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState,useRef } from 'react'
+import './App.css'
+
+const Dictionary = [
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  { word: "Component", meaning: "A reusable building block in React." },
+  { word: "State", meaning: "An object that stores data for a component." }
+]
+
+function App() {
+  const [def, setDef] = useState("")
+  const inputRef = useRef(null);
+
+  const searchDict = () => {
+    let searchText = inputRef.current.value
+    let foundValue = Dictionary.find(dict => dict.word.toLowerCase() === searchText.toLowerCase())?.meaning
+    if(foundValue)
+    setDef(foundValue)
+    else
+    setDef("Word not found in the dictionary.")
+  }
+
+
+  return (
+    <>
+      <h1>Dictionary App</h1>
+      
+        <input type="text" name="search" id="search" ref={inputRef}/>
+        <button onClick={() => searchDict()}>Search</button>
+      
+      <p className="read-the-docs">
+        Definition : 
+      </p>
+
+      {def}
+
+    </>
+  )
+}
+
+export default App
